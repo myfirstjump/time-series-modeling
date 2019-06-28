d63ff7d7d51d442cf59b048aa22f62cac08ef042
edward <edwardchen@phalanxbiotech.com>
2019-06-28 18:05:44 +0800

    update as.Date to time column

--- flu_from_clinic.R
+++ flu_from_clinic.R
@@ -24,7 +24,23 @@ data <- data[remain_idx,]
 sapply(data, class)
 agg_data <- data[, .(flu=sum(類流感健保就診人次), total=sum(健保就診總人次)), by = .(年, 週)]
 agg_data$percentage <- with(agg_data, flu/total)
-
+agg_data$time <- with(agg_data, paste0(年,'-',週))
 ### 3.
 ## https://medium.com/r-%E8%AA%9E%E8%A8%80%E8%87%AA%E5%AD%B8%E7%B3%BB%E5%88%97/r%E8%AA%9E%E8%A8%80%E8%87%AA%E5%AD%B8%E6%97%A5%E8%A8%98-10-%E6%99%82%E9%96%93%E5%BA%8F%E5%88%97%E9%A0%90%E6%B8%AC%E6%96%B9%E6%B3%95-5aef00a9c997
-plot(agg_data$年, agg_data$flu)
+plot(seq(1, length(agg_data$flu)), agg_data$flu, type = 'l')
+
+vector_to_2nchar_elements <- function(vec) {
+  new_v <- c()
+  for (i in 1:length(vec)){
+    if(nchar(vec[i]) == 1) {
+      new_v <- c(new_v, paste0('0',vec[i]))
+    } else {
+      new_v <- c(new_v, as.character(vec[i]))
+    }
+  }
+  return(new_v)
+}
+agg_data$week_string <- vector_to_2nchar_elements(agg_data$週)
+
+as.Date(paste0(agg_data$年, agg_data$週, '星期六'), "%Y%U%a")
+tail(agg_data$週, 20)
